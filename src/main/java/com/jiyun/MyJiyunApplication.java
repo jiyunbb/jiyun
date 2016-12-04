58999dcccf2860a9a74b3aa44c30977d6c436560
WangJiyun <wangjiyun@WangJiyunui-MacBook-Pro.local>
2016-12-04 15:04:45 +0900

    init git

---
+++ src/main/java/com/jiyun/MyJiyunApplication.java
@@ -0,0 +1,12 @@
+package com.jiyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJiyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJiyunApplication.class, args);
+	}
+}
